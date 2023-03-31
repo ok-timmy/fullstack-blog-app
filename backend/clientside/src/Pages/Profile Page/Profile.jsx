9f7aa2819cfdfd39c1316d67a3b00c8cc0d7ec9e
Timilehin Okunola <okunolatimilehin4123@gmail.com>
2023-03-31 23:23:30 +0100

    Sorted Profile Page

--- backend/clientside/src/Pages/Profile Page/Profile.jsx
+++ backend/clientside/src/Pages/Profile Page/Profile.jsx
@@ -6,91 +6,82 @@ import { Link } from "react-router-dom";
 import { useEffect } from "react";
 import { useState } from "react";
 import axios from "axios";
+import { useSelector } from "react-redux";
+import { useGetUserDetailsQuery } from "../../Redux/Auth/authApiSlice";
+import { setCurrentUser } from "../../Redux/Auth/authSlice";
 // import axiosInstance from "../../config";
 
 function Profile() {
-  const { user } = useContext(Context);
-  // console.log(user);
-
-  const pf = "http://localhost:8000/public/";
   const [userPosts, setuserPosts] = useState();
+  const user = useSelector(setCurrentUser);
+  const {
+    data: userDetails,
+    isLoading,
+    isError,
+    isSuccess,
+  } = useGetUserDetailsQuery(user.email);
+  console.log(userDetails);
 
-  const { image, firstName, secondName, userName, email, bio } = user;
+  if (isLoading) {
+    return <div>Loading...</div>;
+  } else if (isError) {
+    return <div>Sorry, Something went wrong, please try again...</div>;
+  } else {
+    const { firstName, secondName, image, userName, email, bio } = userDetails;
 
-  useEffect(() => {
-    const fetchUserPosts = async (email) => {
-      const { data } = await axios.get(
-        "http://localhost:8000/api/post/allposts"
-      );
-      const currentUserPost = data.filter((d) => {
-        return d.authorEmail === email;
-      });
-      console.log(currentUserPost);
-      if (currentUserPost === []) {
-        setuserPosts("no posts");
-      } else {
-        setuserPosts(currentUserPost);
-      }
-    };
-
-    fetchUserPosts(user.email);
-  }, []);
-
-  console.log(userPosts);
-
-  return (
-    <div className="profile">
-      <div className="profile-pic">
-        {image ? (
-          <img src={pf + image} alt="profile-pic" />
-        ) : (
-          <img src={avatar} alt="profile-pic" />
-        )}
-      </div>
-      <div className="profile-details">
-        <div>
-          <label>First Name: </label>
-          <span>{firstName}</span>
-        </div>
-        <div>
-          <label>Last Name: </label>
-          <span>{secondName}</span>
-        </div>
-        <div>
-          <label>Username: </label>
-          <span>{userName}</span>
-        </div>
-        <div>
-          <label>Email Address: </label>
-          <span>{email}</span>
-        </div>
-        <div>
-          <label>Short Bio:</label>
-          <span>{bio || "No Bio Yet."}</span>
-        </div>
-      </div>
-
-      <button className="edit-profile-btn">
-        <Link to={"/edit-profile"} className="edit-link">
-          Edit Profile
-        </Link>
-      </button>
-      <div>
-        <h3>Some of your Works</h3>
-        <div>
-          {!userPosts ? (
-            <div class="loader"></div>
-          ) : userPosts === null ? (
-            userPosts.map((userPost) => {
-              return <div>{userPost.title}</div>;
-            })
+    return (
+      <div className="profile">
+        <div className="profile-pic">
+          {image ? (
+            <img src={image} alt="profile-pic" />
           ) : (
-            <div>You Dont have any post</div>
+            <img src={avatar} alt="profile-pic" />
           )}
         </div>
+        <div className="profile-details">
+          <div>
+            <label>First Name: </label>
+            <span>{firstName}</span>
+          </div>
+          <div>
+            <label>Last Name: </label>
+            <span>{secondName}</span>
+          </div>
+          <div>
+            <label>Username: </label>
+            <span>{userName}</span>
+          </div>
+          <div>
+            <label>Email Address: </label>
+            <span>{email}</span>
+          </div>
+          <div>
+            <label>Short Bio:</label>
+            <span>{bio || "No Bio Yet."}</span>
+          </div>
+        </div>
+        <button className="edit-profile-btn">
+          <Link to={"/edit-profile"} className="edit-link">
+            Edit Profile
+          </Link>
+        </button>
+        <div>
+          <h3>Some of your Works</h3>
+          <div>
+            {!userPosts ? (
+              <div className="loader"></div>
+            ) : userPosts === null ? (
+              userPosts.map((userPost) => {
+                return <div>{userPost.title}</div>;
+              })
+            ) : (
+              <div>You Dont have any post</div>
+            )}
+          </div>
+        </div>
       </div>
-    </div>
-  );
+    );
+  }
 }
 
 export default Profile;
--- backend/clientside/src/Redux/Auth/authApiSlice.js
+++ backend/clientside/src/Redux/Auth/authApiSlice.js
@@ -1,31 +1,40 @@
-import {apiSlice} from "../../App/api/apiSlice"
+import { apiSlice } from "../../App/api/apiSlice";
 
 export const authApiSlice = apiSlice.injectEndpoints({
-    endpoints: (builder) => ({
-        signIn: builder.mutation({
-          query : (credentials) => ({
-            url: "api/auth/login",
-            method: "POST",
-            body: {...credentials}
-          }) 
-        }),
+  endpoints: (builder) => ({
+    signIn: builder.mutation({
+      query: (credentials) => ({
+        url: "api/auth/login",
+        method: "POST",
+        body: { ...credentials },
+      }),
+    }),
 
-        signUp: builder.mutation({
-            query: (credentials) => ({
-                url: "api/auth/register",
-                method: "POST",
-                body: {...credentials}
-            }) 
-        }),
+    signUp: builder.mutation({
+      query: (credentials) => ({
+        url: "api/auth/register",
+        method: "POST",
+        body: { ...credentials },
+      }),
+    }),
 
-        signOut: builder.mutation({
-            query: () => ({
-                url: "api/logout",
-                method: "GET",
-            })
-        })
-    })
-})
+    getUserDetails: builder.query({
+      query: (email) => `api/auth/${email}`,
+      keepUnusedDataFor: 5,
+    }),
 
+    signOut: builder.mutation({
+      query: () => ({
+        url: "api/logout",
+        method: "GET",
+      }),
+    }),
+  }),
+});
 
-export const {useSignInMutation, useSignUpMutation, useSignOutMutation } = authApiSlice;
+export const {
+  useSignInMutation,
+  useSignUpMutation,
+  useSignOutMutation,
+  useGetUserDetailsQuery,
+} = authApiSlice;
